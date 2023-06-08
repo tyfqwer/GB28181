3d2a3175c9561f7d180cae5a0a1bd1503ae0791d
陶乙峰 <1442282810@qq.com>
2023-06-08 22:12:17 +0800

    临时

--- Test.py
+++ Test.py
@@ -1,11 +1,12 @@
 import socket
 
-host = '127.0.0.1'  # 服务端ip
-port = 5060  # 服务端ip端口
+host = '192.168.1.10'  # 服务端ip
+port = 5062  # 服务端ip端口
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 创建socket对象
     sock.bind((host, port))  # 根据服务端地址，建立连接
+    sock.sendto('test'.encode(), ('192.168.1.10', 5061))
     while True:
         data, addr = sock.recvfrom(1024)  # 接收数据
         msg = data.decode()  # 数据从byte格式转出为str
--- sip_client.py
+++ sip_client.py
@@ -1,23 +1,26 @@
-import socket
+import socket, random
 
 # 定义源IP地址和端口
-src_ip = '127.0.0.1'
+src_ip = '192.168.1.10'
 src_port = 5061
 
-host = '127.0.0.1'  # 服务端ip
+host = '192.168.1.10'  # 服务端ip
 port = 5060  # 服务端ip端口
 
+n = 0
 
 def register(s):
-    str_send = 'REGISTER sip:34020000002000000001@3402000000 SIP/2.0\n'
-    str_send += 'Via: SIP/2.0/UDP 192.168.1.120:5060;rport;branch=z9hG4bK1443727699\n'
-    str_send += 'From: <sip:34020000001320000002@3402000000>;tag=474931941\n'
-    str_send += 'To: <sip:34020000001320000002@3402000000>\n'
-    str_send += 'Call-ID: 732886928\n'
-    str_send += 'CSeq: 1 REGISTER\n'
-    str_send += 'Contact: <sip:34020000001320000002@192.168.1.120:5060>\n'
+    global n
+    str_send = 'REGISTER sip:34020000002000000001@{}:{} SIP/2.0\n'.format(host, port)
+    str_send += 'Via: SIP/2.0/UDP {}:{};rport;branch=z9hG4bK{}\n'.format(src_ip, src_port, str(random.randint(10000000000, 99999999999))[1:])
+    str_send += 'From: <sip:34020000001320001234@{}:{}>;tag={}\n'.format(src_ip, src_port, str(random.randint(1000000000, 9999999999))[1:])
+    str_send += 'To: <sip:34020000001320001234@{}:{}>\n'.format(src_ip, src_port)
+    str_send += 'Call-ID: {}\n'.format(str(random.randint(1000000000, 9999999999))[1:])
+    n += 1
+    str_send += 'CSeq: {} REGISTER\n'.format(str(n))
+    str_send += 'Contact: <sip:34020000001320001234@{}:{}>\n'.format(src_ip, src_port)
     str_send += 'Max-Forwards: 70\n'
-    str_send += 'User-Agent: IP Camera\n'
+    str_send += 'User-Agent: NCG V2.6.3.777777\n'
     str_send += 'Expires: 3600\n'
     str_send += 'Content-Length: 0\n\n'
     print(str_send)
